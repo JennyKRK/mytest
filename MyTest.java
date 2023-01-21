d193f68165ff05d8e3670dc5e3528dfcd87224b2
Joanna N <joanna.n@student.agh.edu.pl>
2023-01-21 23:34:38 +0100

    The first version

---
+++ MyTest.java
@@ -0,0 +1,11 @@
+public class MyTest { 
+    public static void main(String[] args) {
+      System.out.println("This is only a test"); 
+    }
+	
+
+	public static void sayHello(){
+		System.out.println("Hello everybody");
+}
+
+}
